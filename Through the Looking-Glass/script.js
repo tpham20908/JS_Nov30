b42338b436107d29d9964039bd5a3cda8694ffe1
Pham <1796137@johnabbottcollege.net>
2017-11-30 12:04:51 -0500

    finish Looking-Glass exercise

---
+++ Through the Looking-Glass/script.js
@@ -0,0 +1,10 @@
+// javascript file for Looking-Glass web page
+$(function() {
+	$('.poem-stanza').addClass('highlight');
+	// var poem = $('.poem');
+	// alert(poem.length);
+	// var para1 = $('#fred');
+	// var para2 = $('#.poem .poem-title .poem-stanza:last');
+	// para1.addClass('highlight');
+	// para1.addClass('highlight');
+})
